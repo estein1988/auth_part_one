c6d1d43527dc3ea8c92a2926b6f4e1549637f082
estein1988 <estein1988@gmail.com>
2020-09-22 16:01:40 -0600

    Auth part two.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def index
         @users = User.all 
         render json: @users
@@ -13,4 +12,19 @@ class UsersController < ApplicationController
         render json: @user
     end
 
+    def login
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode( {user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            render json: {user: @user, token: @token}
+        else
+            render json: {message: "Invalid username or password!"}
+        end
+    end
 end
+
+# @@attempts = 0
+# @@attempts += 1
+# elsif @@atempts > 2
+# render json: {message: "Too many requests!"}
