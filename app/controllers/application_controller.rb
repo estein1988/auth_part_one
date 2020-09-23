4d20fc557f78120e4f35519a54717445fedadef3
estein1988 <estein1988@gmail.com>
2020-09-23 12:50:01 -0600

    Auth part 3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+
+    before_action :authorized
+
+    def authorized
+        render json: {message: "Please Log In"} unless logged_in
+    end
+
+    def logged_in
+        !!current_user 
+    end
+
+    def current_user
+        auth_header = request.headers["Authorization"]
+        if auth_header
+            token = auth_header.split(" ")[1]
+            begin
+                @user_id = JWT.decode(token, secret_key)[0]["user_id"]
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+        @user = User.find(@user_id)
+    end
+
+    def secret_key
+        Rails.application.secrets.secret_key_base[0]
+    end
+
 end
+
+#double bang turns into a boolean
+#if logged_in is true, do no hit the authorized method
+
+#rescue avoids entire app shutting down if one user messess up a token
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+    
+    skip_before_action :authorized, only: [:create, :login]
+    
     def index
         @users = User.all 
         render json: @users
+    end
+
+    def profile
+        render json: @user
     end
 
     def create
@@ -16,7 +23,7 @@ class UsersController < ApplicationController
         @user = User.find_by(username: params[:username])
 
         if @user && @user.authenticate(params[:password])
-            @token = JWT.encode( {user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            @token = JWT.encode( {user_id: @user.id}, Rails.application.secrets.secret_key_base[0] )
             render json: {user: @user, token: @token}
         else
             render json: {message: "Invalid username or password!"}
